538431c994e5253ec7e4f214cf0883bb2ee5d48d
Hallel <hallelputter@gmail.com>
2025-06-23 16:42:33 +0300

    add pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,24 @@
+import Layout from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NoPage } from "./pages/NoPage";
+import { NewPost } from "./pages/NewPost";
+import { Profile } from "./pages/Profile";
+
 function App() {
-  
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="new-post" element={<NewPost />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ Frontend/src/pages/NewPost/NewPost.tsx
@@ -0,0 +1,3 @@
+export const NewPost = () => {
+  return <h1>New Post</h1>;
+};
