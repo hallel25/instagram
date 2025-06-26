a06b225f079984b0be008089c38bae9ded8e8ffc
hallel <halelputter@gmail.com>
2025-06-26 11:45:52 +0300

    add edit post dto

---
+++ Backend/src/post/dto/editPost.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString, IsUrl } from "class-validator";
+
+export class editPostDto {
+    @IsString()
+    caption: string;
+
+    @IsUrl()
+    @IsNotEmpty({ message: "image url can't be empty"})
+    imageUrl: string;
+
+    constructor(caption: string, imageUrl: string) {
+        this.caption = caption;
+        this.imageUrl = imageUrl;
+    }
+}
