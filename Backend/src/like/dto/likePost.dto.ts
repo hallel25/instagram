fff027a953b05701bb20bd1a01206def45280df5
hallel <halelputter@gmail.com>
2025-06-26 17:28:41 +0300

    add like post dto

---
+++ Backend/src/like/dto/likePost.dto.ts
@@ -0,0 +1,21 @@
+import { IsDate, IsNotEmpty, IsString, IsUUID, IsUrl } from "class-validator";
+import { UUID } from "crypto";
+
+export class likePostDto {
+    @IsDate()
+    createdAt: Date;
+
+    @IsUUID()
+    @IsNotEmpty({ message: "post id can't be emtpy"})
+    postId: UUID;
+
+    @IsUUID()
+    @IsNotEmpty({ message: "user id can't be emtpy"})
+    userId: UUID;
+
+    constructor(createdAt: Date, postId: UUID, userid: UUID) {
+        this.createdAt = createdAt;
+        this.postId = postId;
+        this.userId = userid;
+    }
+}
