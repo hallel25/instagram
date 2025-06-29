56f70639ed067418e8bbeea171b76889826e2f9d
hallel <halelputter@gmail.com>
2025-06-29 10:03:51 +0300

    fix upper navbar

--- Frontend/src/components/Navbar/Navbar.tsx
+++ Frontend/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {
+  Box,
   Paper,
   Typography,
 } from "@mui/material";
@@ -15,38 +16,45 @@ export const Navbar: React.FC<NavbarProps> = ({ text, canExit }) => {
   const navigate = useNavigate();
 
   return (
-    <Paper
-      sx={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        background: "white",
-        height: 50,
-        display: "flex",
-        alignItems: "center",
-      }}
-      elevation={3}
-      square
-    >
-      {canExit && (
-        <button
-          onClick={() => navigate(-1)}
-          style={{ background: "white", border: 0 }}
-        >
-          <CloseIcon />
-        </button>
-      )}
-      <Typography
-        variant="h4"
+    <>
+      <Paper
         sx={{
-          color: "black",
-          fontFamily: "Lavishly Yours",
-          textAlign: "center",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          background: "white",
+          height: 50,
+          display: "flex",
+          alignItems: "center",
         }}
+        elevation={3}
+        square
       >
-        {text}
-      </Typography>
-    </Paper>
+        {canExit && (
+          <button
+            onClick={() => navigate(-1)}
+            style={{
+              background: "white", border: 0, alignItems: "center", height: "100%", display: "flex"
+            }}
+          >
+            <CloseIcon />
+          </button>
+        )}
+        <Typography
+          variant="h4"
+          sx={{
+            color: "black",
+            fontFamily: "Send Flowers",
+            textAlign: "center",
+            flexGrow: 1,
+          }}
+        >
+          {text}
+        </Typography>
+        {canExit && <div style={{ width: 35 }} />}
+      </Paper>
+      <Box sx={{ height: 40 }} />
+    </>
   );
 };
