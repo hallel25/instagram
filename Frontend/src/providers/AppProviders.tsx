7968a487ad59bfce661e6313da4d457c6e1de0de
hallel <halelputter@gmail.com>
2025-07-01 17:30:09 +0300

    add queryClient

---
+++ Frontend/src/providers/AppProviders.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "../lib/queryClient";
+import { Provider as JotaiProvider } from "jotai";
+
+interface AppProvidersProps {
+  children: React.ReactNode;
+}
+
+const AppProviders: React.FC<AppProvidersProps> = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <JotaiProvider>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </JotaiProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default AppProviders;
