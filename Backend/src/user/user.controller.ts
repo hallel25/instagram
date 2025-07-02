2a402e381c475ae5984c3a31b9bc5c6c7c2892d2
hallel <halelputter@gmail.com>
2025-07-02 17:26:54 +0300

    add get user by id

--- Backend/src/user/user.controller.ts
+++ Backend/src/user/user.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
-import { UserService } from "./user.service";
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UUID } from 'crypto';
 
 @Controller('users')
 export class UserController {
-    constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {}
 
-    @Get()
-    getAll() {
-        return this.userService.getAllUsers();
-    }
+  @Get()
+  getAll() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':userId')
+  async getUserById(@Param('userId') userId: UUID) {
+    return this.userService.getUsersById(userId);
+  }
 }
--- Backend/src/user/user.service.ts
+++ Backend/src/user/user.service.ts
@@ -1,22 +1,32 @@
-import { UUID } from "crypto";
-import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from "./entities/user.entity";
-import { mockUsers } from "src/DB/DB";
+import { User } from './entities/user.entity';
+import { mockUsers } from 'src/DB/DB';
 
 export abstract class IUserService {
-    abstract getAllUsers(): User[];
+  abstract getAllUsers(): User[];
+  abstract getUsersById(userId: string): User;
 }
 
 @Injectable()
 export class UserService implements IUserService {
-    // constructor(
-    //     @InjectRepository(User)
-    //     private UserRepository: Repository<User>
-    // ) {}
+  // constructor(
+  //     @InjectRepository(User)
+  //     private UserRepository: Repository<User>
+  // ) {}
 
-    getAllUsers(): User[] {
-        return mockUsers;
+  getAllUsers(): User[] {
+    return mockUsers;
+  }
+
+  getUsersById(userId: string): User {
+    const user = mockUsers.find((user) => user.id == userId);
+
+    if (user) {
+      return user;
+    } else {
+      throw new Error('post doesnt exist');
     }
+  }
 }
