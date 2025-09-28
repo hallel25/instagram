257439f72ba62bcd4ce00139fc2f3ab406112a34
Niv Shirazi <37215546+NivShirazi@users.noreply.github.com>
2025-09-28 13:37:18 +0300

    delete unnecessary app files

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { userModule } from './post/post.module';
 import { postModule } from './user/user.module';
 import { likeModule } from './like/like.module';
 
 @Module({
   imports: [userModule, postModule, likeModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
